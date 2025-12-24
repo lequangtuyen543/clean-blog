7df1b5820280ba6edbd2e1a68af7c36dbda1f704
Le Quang Tuyen <124868890+lequangtuyen543@users.noreply.github.com>
2025-12-24 18:27:01 +0700

    2. Xong Hero

--- reactjs/src/layout/client/Header.js
+++ reactjs/src/layout/client/Header.js
@@ -13,85 +13,101 @@ export const Header = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <header className="bg-white header">
-      <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
-        <div className="flex lg:flex-1">
-          <a href="/" className="-m-1.5 p-1.5">
-            <span className="logo">Start Bootstrap</span>
-          </a>
-        </div>
-        <div className="flex lg:hidden">
-          <button
-            type="button"
-            onClick={() => setMobileMenuOpen(true)}
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-          >
-            <span className="sr-only">Menu</span>
-            <Bars3Icon aria-hidden="true" className="size-6" />
-          </button>
-        </div>
-        <PopoverGroup className="hidden lg:flex lg:gap-x-12">
-          <a href="/" className="text-sm/6 font-semibold text-gray-900">
-            Home
-          </a>
-          <a href="/about" className="text-sm/6 font-semibold text-gray-900">
-            About
-          </a>
-          <a href="/sample-post" className="text-sm/6 font-semibold text-gray-900">
-            Sample Post
-          </a>
-          <a href="/contact" className="text-sm/6 font-semibold text-gray-900">
-            Contact
-          </a>
-        </PopoverGroup>
-      </nav>
-      <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
-        <div className="fixed inset-0 z-50" />
-        <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-          <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
+    <div className="bg-white page-section">
+      <header className="bg-white header">
+        <nav aria-label="Global" className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8">
+          <div className="flex lg:flex-1">
+            <a href="/" className="-m-1.5 p-1.5">
+              <span className="logo">Start Bootstrap</span>
             </a>
+          </div>
+          <div className="flex lg:hidden">
             <button
               type="button"
-              onClick={() => setMobileMenuOpen(false)}
-              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              onClick={() => setMobileMenuOpen(true)}
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
             >
-              <span className="sr-only">Close menu</span>
-              <XMarkIcon aria-hidden="true" className="size-6" />
+              <span className="sr-only">Menu</span>
+              <Bars3Icon aria-hidden="true" className="size-6" />
             </button>
           </div>
-          <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/10">
-              <div className="space-y-2 py-6">
-                <a
-                  href="/"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
-                >
-                  Home
-                </a>
-                <a
-                  href="/about"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
-                >
-                  About
-                </a>
-                <a
-                  href="/sample-post"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
-                >
-                  Sample Post
-                </a>
-                <a
-                  href="/contact"
-                  className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
-                >
-                  Contact
-                </a>
+          <PopoverGroup className="hidden lg:flex lg:gap-x-12">
+            <a href="/" className="text-sm/6 font-semibold text-gray-900">
+              Home
+            </a>
+            <a href="/about" className="text-sm/6 font-semibold text-gray-900">
+              About
+            </a>
+            <a href="/sample-post" className="text-sm/6 font-semibold text-gray-900">
+              Sample Post
+            </a>
+            <a href="/contact" className="text-sm/6 font-semibold text-gray-900">
+              Contact
+            </a>
+          </PopoverGroup>
+        </nav>
+        <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
+          <div className="fixed inset-0 z-50" />
+          <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+            <div className="flex items-center justify-between">
+              <a href="#" className="-m-1.5 p-1.5">
+              </a>
+              <button
+                type="button"
+                onClick={() => setMobileMenuOpen(false)}
+                className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              >
+                <span className="sr-only">Close menu</span>
+                <XMarkIcon aria-hidden="true" className="size-6" />
+              </button>
+            </div>
+            <div className="mt-6 flow-root">
+              <div className="-my-6 divide-y divide-gray-500/10">
+                <div className="space-y-2 py-6">
+                  <a
+                    href="/"
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                  >
+                    Home
+                  </a>
+                  <a
+                    href="/about"
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                  >
+                    About
+                  </a>
+                  <a
+                    href="/sample-post"
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                  >
+                    Sample Post
+                  </a>
+                  <a
+                    href="/contact"
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
+                  >
+                    Contact
+                  </a>
+                </div>
               </div>
             </div>
+          </DialogPanel>
+        </Dialog>
+      </header>
+
+      <div className="relative isolate px-6 pt-14 lg:px-8 hero">
+        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+          <div className="text-center">
+            <h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
+              Clean Blog
+            </h1>
+            <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
+              A Blog Theme by Start Bootstrap
+            </p>
           </div>
-        </DialogPanel>
-      </Dialog>
-    </header>
+        </div>
+      </div>
+    </div>
+
   )
 }
