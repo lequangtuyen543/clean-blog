c521a19ed227fefa6b7720691db3da22952c29e3
Le Quang Tuyen <124868890+lequangtuyen543@users.noreply.github.com>
2025-12-24 17:34:06 +0700

    1. Xong Header

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -5,6 +5,9 @@ function App() {
   return (
     <>
       <AllRoute />
+      {/* <div className="bg-red-500 text-white p-10 text-3xl">
+        Tailwind đang chạy
+      </div> */}
     </>
   );
 }
